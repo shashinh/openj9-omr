64bd3429e1288a0cc48b66de84d6de2921b731af
Shashin Halalingaiah <shashin.halalingaiah@gmail.com>
2022-05-11 09:09:42 +0000

    backup

--- compiler/optimizer/ptgparser/structs.h
+++ compiler/optimizer/ptgparser/structs.h
@@ -39,4 +39,9 @@ struct Entry {
     }
 };
 
+//this enables usage of an Entry as a key to a sigma set
+bool operator<(const Entry& x, const Entry& y) {
+    return x.caller != y.caller ? x.caller < y.caller : x.bci < y.bci;
+}
+
 #endif
